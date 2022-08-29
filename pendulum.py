e004f5ea461e04e7c82eaeda5819caf2366b1e2d
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-08-29 17:08:42 +0200

    pygame test working with damping term in eom

--- pendulum.py
+++ pendulum.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
 import abc
 from abc import ABC, abstractmethod
 import numpy as np
 import pygame
+
+screenw = 600
+screenh = 600
 
 class pendulum(ABC) :
     @abstractmethod
@@ -24,20 +29,30 @@ class pendulum1M(pendulum) :
         self._omega = omega
         self._alpha = self.eom()
         self._x = self._length * np.sin(self._theta)
-        self._y = - self._length * np.cos(self._theta) 
+        self._y = -self._length * np.cos(self._theta)
+
 
     def eom(self) :
-        return -9.81*np.sin(self._theta)/self._length
+        # beta = 0.01, g = 9.81 (world properties)
+        return -9.81*np.sin(self._theta)/self._length - 0.1*self._omega
 
     def Draw(self) :
-        # not implemented yet #
-        print("Drawing is not implemented yet.")
+        # pass to function
+        x = self._x * (0.5*screenw/self._length) + screenw/2
+        y = -self._y * (0.5*screenh/self._length) + screenh/10
+        # pygame test
+        screen.fill((255,255,255))
+        self._massrect = pygame.draw.circle(screen, (0,0,255), (x, y), 20, 5) #(r, g, b) is color, (x, y) is center, (R, w) is radius and width.
+        pygame.display.update()
+        
 
 class pendulum1MEuler(pendulum1M) :
     def Update(self, dt) :
         self._theta = self._theta + self._omega*dt + 0.5*self._alpha*dt*dt
         self._omega = self._omega + self._alpha*dt
         self._alpha = self.eom()
+        self._x = self._length * np.sin(self._theta)
+        self._y = -self._length * np.cos(self._theta) 
         
 
     def Print(self) :
@@ -45,23 +60,22 @@ class pendulum1MEuler(pendulum1M) :
         
 
 if __name__ == "__main__" :
-    pend = pendulum1MEuler(1, 1, np.pi/4, 0)
-
-    for t in range(10) :
-        pend.Print()
-        pend.Update(0.1)
+    # pend = pendulum1MEuler(1, 1, np.pi/4, 0)
     
     # pygame test
     pygame.init()
-    screen = pygame.display.set_mode((600, 400)) #x and y are height and width
+    screen = pygame.display.set_mode((screenw, screenh))
 
     pygame.display.set_caption("Pendulum Simulation")
     screen.fill((255,255,255))
-    pygame.draw.circle(screen, (0,0,255), (0, 0), 20, 5) #(r, g, b) is color, (x, y) is center, (R, w) is radius and width.
-    pygame.display.update()
+
+    pend = pendulum1MEuler(1, 1, np.pi/4, 0)
 
     running = True
     while running:
+        pend.Draw()
+        pend.Update(0.001)
+        
         ev = pygame.event.get()
 
         for event in ev:
