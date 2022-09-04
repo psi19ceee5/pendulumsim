188c107b888d63aecfd29d6a2064a4dcc28eb3c9
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-09-04 22:36:50 +0200

    New base class drawable

--- pendulum.py
+++ pendulum.py
@@ -8,17 +8,18 @@ import pygame
 screenw = 600
 screenh = 600
 
-class pendulum(ABC) :
+class drawable(ABC) :
     @abstractmethod
-    def __init__(self) :
+    def draw(self) :
         pass
 
     @abstractmethod
-    def Draw(self) :
+    def update(self) :
         pass
 
+class pendulum(drawable) :
     @abstractmethod
-    def Update(self) :
+    def eom(self) :
         pass
 
 class pendulum1M(pendulum) :
@@ -31,33 +32,29 @@ class pendulum1M(pendulum) :
         self._x = self._length * np.sin(self._theta)
         self._y = -self._length * np.cos(self._theta)
 
-
     def eom(self) :
         # beta = 0.01, g = 9.81 (world properties)
         return -9.81*np.sin(self._theta)/self._length - 0.1*self._omega
 
-    def Draw(self) :
+    def draw(self) :
         # pass to function
         x = self._x * (0.5*screenw/self._length) + screenw/2
         y = -self._y * (0.5*screenh/self._length) + screenh/10
         # pygame test
         screen.fill((255,255,255))
         self._massrect = pygame.draw.circle(screen, (0,0,255), (x, y), 20, 5) #(r, g, b) is color, (x, y) is center, (R, w) is radius and width.
-        pygame.display.update()
-        
+        pygame.display.update()        
 
 class pendulum1MEuler(pendulum1M) :
-    def Update(self, dt) :
+    def update(self, dt) :
         self._theta = self._theta + self._omega*dt + 0.5*self._alpha*dt*dt
         self._omega = self._omega + self._alpha*dt
         self._alpha = self.eom()
         self._x = self._length * np.sin(self._theta)
-        self._y = -self._length * np.cos(self._theta) 
-        
+        self._y = -self._length * np.cos(self._theta)         
 
-    def Print(self) :
-        print(self._theta, self._omega, self._alpha)
-        
+    def print(self) :
+        print(self._theta, self._omega, self._alpha)        
 
 if __name__ == "__main__" :
     # pend = pendulum1MEuler(1, 1, np.pi/4, 0)
@@ -73,8 +70,8 @@ if __name__ == "__main__" :
 
     running = True
     while running:
-        pend.Draw()
-        pend.Update(0.001)
+        pend.draw()
+        pend.update(0.001)
         
         ev = pygame.event.get()
 
